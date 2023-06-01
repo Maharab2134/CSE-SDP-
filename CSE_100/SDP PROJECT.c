910d10a2d456cb6e9d599aec3c8e9ea24f0f778f
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-06-01 17:37:27 +0600

    Add files via upload

--- CSE_100/SDP PROJECT.c
+++ CSE_100/SDP PROJECT.c
@@ -1,28 +1,30 @@
-//header file for standard input and output
-#include<stdio.h> 
+#include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
-//structure to store items
-struct items{ 
-    char item[20];
-    float price;
-    int qty;
+#include<stdlib.h> // conversions, memory allocation, algorithms, and other similar use this hedder file
+
+struct items{
+     int qty;
+     char item[20];
+     float price;
 };
-struct orders{ 
-    char customer[50];
-    char date[50];
-    int numOfItems;
+
+struct orders{
+     int numOfItems;
+     char customer[50];
+     char date[50];
+
 struct items itm[50];
 };
-//function to print bill header
-    void BillHeader
-    (
+
+    void BillHeader(
+
         char name[50],char date[30]
-    )
+
+        )
 
     {
         printf("\n\n");
-        printf("\t    AKASH Restaurant");
+        printf("\t    Friend Club Restaurant");
         printf("\n\t   -----------------");
         printf("\nDate:%s",date);
         printf("\nInvoice To: %s",name);
@@ -34,54 +36,52 @@ struct items itm[50];
         printf("\n---------------------------------------");
         printf("\n\n");
     }
-//function to print bill body
-        void BillBody
-        (
-            char item[30],int qty, float price){
-            printf("%s\t\t",item); 
-            printf("%d\t\t",qty); 
-            printf("%.2f\t\t",qty * price); 
+
+        void BillBody(
+
+            char item[30],
+            int qty,
+            float price
+            ){
+            printf("%s\t\t",item);
+            printf("%d\t\t",qty);
+            printf("%.2f\t\t",qty * price);
             printf("\n");
-        }
-    //function to print bill footer
-
-        void BillFooter(float total)
-           {
-                printf("\n");
-                float netTotal=total;
-                printf("---------------------------------------\n");
-                printf("Sub Total\t\t\t%.2f",total);
             }
-    // main function
 
-        int main()
-            {
+        void BillFooter(float total){
+          printf("\n");
+          float netTotal=total;
+          printf("---------------------------------------\n");
+          printf("Sub Total\t\t\t%.2f",total);
+          }
+
+        int main(){
+
                 int opt,n;
                 struct orders ord;
                 struct orders order;
-                char saveBill = 'y',contFlag = 'y', name[50];
+                char saveBill = 'y', contFlag = 'y';
+                char name[50];
                 FILE *fp;
-
-    //loop to display menu
-                while(contFlag == 'y')
-           {
+                while(contFlag == 'y'){
                 system("clear");
                 float total = 0;
                 int invoiceFound = 0;
-                printf("\t============AKASH RESTAURANT============");
+                printf("\t============Friends Cafe RESTAURANT============");
                 printf("\n\nPlease select your prefered operation");
                 printf("\n\n1.Generate Invoice");
                 printf("\n2.Show all Invoices");
                 printf("\n3.Search Invoice");
                 printf("\n4.Exit");
-            
                 printf("\n\nYour choice:\t");
                 scanf("%d",&opt);
                 fgetc(stdin);
-                //switch case to perform operations
+
                 switch(opt){
                 case 1:
                 system("clear");
+
                 printf("\nPlease enter the name of the customer:\t");
                 fgets(ord.customer,50,stdin);
                 ord.customer[strlen(ord.customer)-1] = 0;
@@ -90,7 +90,9 @@ struct items itm[50];
                 scanf("%d",&n);
                 ord.numOfItems = n;
                for(int i=0;i<n;i++)
+
              {
+
                 fgetc(stdin);
                 printf("\n\n");
                 printf("Please enter the item Name: %d:\t",i+1);
@@ -102,70 +104,79 @@ struct items itm[50];
                 scanf("%f",&ord.itm[i].price);
                 total += ord.itm[i].qty * ord.itm[i].price;
              }
-             //calling functions
+
                 BillHeader(ord.customer,ord.date);
-                for(int i=0;i<ord.numOfItems;i++)
+                for(int i=0;i<ord.numOfItems;i++){
+                BillBody(ord.itm[i].item,ord.itm[i].qty,ord.itm[i].price);
+                }
+
                 BillFooter(total);
-                //saving bill
+
                 printf("\nDo you want to save the bill invoice? (y/n):\t");
                 scanf("%s",&saveBill);
-                //saving bill in file
+
                 if(saveBill == 'y')
-              {
-                    fp = fopen("Restaurant.txt","a");
-                    fwrite(&ord,sizeof(ord),1,fp);
+                {
+                    fp = fopen("Restaurant.txt","a+");
+                    fwrite(&ord,sizeof(struct orders),1,fp);
                     if(fwrite !=0)
                     printf("\nInvoice saved successfully!\n");
                     else
                     printf("\nError saving file!\n");
                     fclose(fp);
                 }
+
                 break;
                 case 2:
+
                   system("clear");
+
                   fp = fopen("Restaurant.txt","r");
                   printf("\n  *****Your Previous Invoices*****\n");
                   while(fread(&order,sizeof(struct orders),1,fp)){
                   float tot=0;
                   BillHeader(order.customer,order.date);
                   for(int i=0;i<order.numOfItems;i++){
+                  BillBody(order.itm[i].item,order.itm[i].qty,order.itm[i].price);
                   tot+=order.itm[i].qty*order.itm[i].price;
                 }
+
                 BillFooter(tot);
             }
+
                 fclose(fp);
                 break;
-                //searching invoice
                 case 3:
-                printf("Enter the name of the customer:\t");
-                //fgets is used to read string with spaces
-               fgets(name,50,stdin);
-               name[strlen(name)-1] = 0;
-               system("clear");
-               fp = fopen("RestaurantBill.dat","r");
-               printf("\t*****Invoice of %s*****",name);
-               while(fread(&order,sizeof(struct orders),1,fp))
-               { 
-                float tot = 0;
-                if(!strcmp(order.customer,name))
-               {
+                    printf("Enter the name of the customer:\t");
+                    fgets(name,50,stdin);
+                    name[strlen(name)-1] = 0;
 
-              BillHeader(order.customer,order.date);
-              for(int i=0;i<order.numOfItems;i++)
+                    system("clear");
+                    fp = fopen("Restaurant.txt","r");
+                    printf("\t*****Invoice of %s*****",name);
+                    while(fread(&order,sizeof(struct orders),1,fp)){
+                    float tot = 0;
+                    if(!strcmp(order.customer,name)){
+                    BillHeader(order.customer,order.date);
+                    for(int i=0;i<order.numOfItems;i++){
+                    BillBody(order.itm[i].item,order.itm[i].qty,order.itm[i].price);
+                    tot+=order.itm[i].qty * order.itm[i].price;
+            }
 
-              {
-                tot+=order.itm[i].qty * order.itm[i].price;   
-              }
-                BillFooter(tot);
-                invoiceFound = 1;
+            BillFooter(tot);
+            invoiceFound = 1;
+
             }
-        }  
-            if(!invoiceFound)
-            {
-                printf("Sorry the invoice for %s doesnot exists",name);
-            }
-            fclose(fp);
-            break;
+
+        }
+
+        if(!invoiceFound)
+        {
+            printf("Sorry the invoice for %s doesnot exists",name);
+        }
+
+        fclose(fp);
+        break;
             case 4:
             printf("\n\t\t Bye Bye :)\n\n");
             exit(0);
@@ -173,9 +184,12 @@ struct items itm[50];
             default:
             printf("Sorry invalid option");
             break;
-    }
-    printf("\nDo you want to perform another operation? [y/n]:\t");
-    scanf("%s",&contFlag);
-    }
-    return 0;
-}
+       }
+
+       printf("\nDo you want to perform another operation? [y/n]:\t");
+       scanf("%s",&contFlag);
+
+       }
+         return 0;
+
+         }
